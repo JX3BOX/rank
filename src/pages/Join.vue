d979d008e16ea00abcc26791c949c26335deedf3
iRuxu <rx6@qq.com>
2020-10-20 15:22:11 +0800

    update

--- src/pages/Join.vue
+++ src/pages/Join.vue
@@ -6,105 +6,100 @@
                 <img class="u-logo" :src="LOGO" />
             </div>
             <div class="m-rank-content" v-if="isLogin">
-                <template v-if="events && events.length">
-                    <div class="m-join m-join-team" v-if="!status">
-                        <h1 class="m-join-title">报名入口</h1>
-                        <el-alert
-                            class="u-warning"
-                            title="请务必正确填写所有资料，报名后不可取消，亦不可再更改资料。团长群：785597424"
-                            type="warning"
-                            show-icon
-                        >
-                        </el-alert>
-                        <el-form
-                            class="m-join-form"
-                            ref="form"
-                            :model="form"
-                            label-width="80px"
-                        >
-                            <el-form-item label="报名活动">
-                                <el-select
-                                    v-model="form.event_id"
-                                    placeholder="请选择活动"
+                <div class="m-join m-join-team" v-if="!status">
+                    <h1 class="m-join-title">报名入口</h1>
+                    <el-alert
+                        class="u-warning"
+                        title="请务必正确填写所有资料，报名后不可取消，亦不可再更改资料。团长群：785597424"
+                        type="warning"
+                        show-icon
+                    >
+                    </el-alert>
+                    <el-form
+                        class="m-join-form"
+                        ref="form"
+                        :model="form"
+                        label-width="80px"
+                    >
+                        <el-form-item label="报名活动">
+                            <el-select
+                                v-model="form.event_id"
+                                placeholder="请选择活动"
+                            >
+                                <el-option
+                                    v-for="event in events"
+                                    :key="event.ID"
+                                    :label="event.name"
+                                    :value="event.ID"
                                 >
-                                    <el-option
-                                        v-for="event in events"
-                                        :key="event.ID"
-                                        :label="event.name"
-                                        :value="event.ID"
-                                    >
-                                    </el-option>
-                                </el-select>
-                            </el-form-item>
-                            <el-form-item label="选择团队">
-                                <el-select
-                                    v-model="form.team_id"
-                                    placeholder="请选择团队"
+                                </el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="选择团队">
+                            <el-select
+                                v-model="form.team_id"
+                                placeholder="请选择团队"
+                            >
+                                <el-option
+                                    v-for="team in teams"
+                                    :key="team.ID"
+                                    :label="team.name"
+                                    :value="team.ID"
                                 >
-                                    <el-option
-                                        v-for="team in teams"
-                                        :key="team.ID"
-                                        :label="team.name"
-                                        :value="team.ID"
-                                    >
-                                    </el-option>
-                                </el-select>
-                                <div class="u-tip" v-if="!teams.length">
-                                    还没有团队？<a
-                                        href="/team/#/org/setting"
-                                        target="_blank"
-                                        >创建团队</a
-                                    >
-                                </div>
-                            </el-form-item>
-                            <el-form-item label="联系电话">
-                                <el-input
-                                    v-model="form.phone"
-                                    placeholder="请输入正确的快递联系电话"
-                                ></el-input>
-                            </el-form-item>
-                            <el-form-item label="邮寄地址">
-                                <el-input
-                                    v-model="form.address"
-                                    placeholder="请输入正确的奖品邮寄地址"
-                                ></el-input>
-                            </el-form-item>
-                            <el-form-item label="联系QQ">
-                                <el-input
-                                    v-model="form.qq"
-                                    placeholder="请输入正确的QQ号码"
-                                ></el-input>
-                            </el-form-item>
-                            <div class="u-btns">
-                                <el-button
-                                    class="u-btn"
-                                    type="primary"
-                                    @click="submit"
-                                    >报名</el-button
+                                </el-option>
+                            </el-select>
+                            <div class="u-tip" v-if="!teams.length">
+                                还没有团队？<a
+                                    href="/team/#/org/setting"
+                                    target="_blank"
+                                    >创建团队</a
                                 >
                             </div>
-                        </el-form>
-                    </div>
-                    <div class="m-join m-join-done" v-else>
-                        <h1 class="u-title">已报名</h1>
-                        <div>
-                            <p>
-                                活动：<b>{{ event_name }}</b>
-                            </p>
-                            <p>
-                                团队：<strong>{{ team_name }}</strong>
-                            </p>
+                        </el-form-item>
+                        <el-form-item label="联系电话">
+                            <el-input
+                                v-model="form.phone"
+                                placeholder="请输入正确的快递联系电话"
+                            ></el-input>
+                        </el-form-item>
+                        <el-form-item label="邮寄地址">
+                            <el-input
+                                v-model="form.address"
+                                placeholder="请输入正确的奖品邮寄地址"
+                            ></el-input>
+                        </el-form-item>
+                        <el-form-item label="联系QQ">
+                            <el-input
+                                v-model="form.qq"
+                                placeholder="请输入正确的QQ号码"
+                            ></el-input>
+                        </el-form-item>
+                        <div class="u-btns">
+                            <el-button
+                                class="u-btn"
+                                type="primary"
+                                @click="submit"
+                                >报名</el-button
+                            >
                         </div>
+                    </el-form>
+                </div>
+                <div class="m-join m-join-done" v-else>
+                    <h1 class="u-title">已报名</h1>
+                    <div>
+                        <p>
+                            活动：<b>{{ event_name }}</b>
+                        </p>
+                        <p>
+                            团队：<strong>{{ team_name }}</strong>
+                        </p>
                     </div>
-                    <div class="u-tip">
-                        <a href="/tool/?pid=18044#/" target="_blank"
-                            ><i class="el-icon-info"></i>
-                            <b>点击查看活动规则详情</b></a
-                        >
-                    </div>
-                </template>
-                <div class="m-join m-join-null" v-else>
-                    <h1>当前没有开启的活动</h1>
+                </div>
+                <div class="u-tip">
+                    <a href="/tool/?pid=18044#/" target="_blank"
+                        ><i class="el-icon-info"></i>
+                        <b>点击查看活动规则详情</b></a
+                    >
                 </div>
             </div>
             <div class="m-rank-content m-rank-null" v-else>
@@ -207,6 +202,7 @@ export default {
             }).then((res) => {
                 this.events = res.data.data.list;
                 this.form.event_id = this.event_id;
+                this.$forceUpdate()
             });
         },
         loadTeams: function() {
@@ -214,17 +210,19 @@ export default {
             return getMyTeams().then((res) => {
                 this.teams = res.data.data.list;
                 this.form.team_id = this.team_id;
+                this.$forceUpdate()
             });
         },
         checkJoin: function() {
             return hasJoined(ACTIVE_EVENT_ID).then((res) => {
                 if (res.data.data.hasJoined) {
                     this.status = 1;
+                    this.$forceUpdate()
                 }
             });
         },
     },
-    mounted: function() {
+    created: function() {
         this.init();
     },
     components: {},
