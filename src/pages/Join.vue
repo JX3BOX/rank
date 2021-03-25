d5d1930e9d59044c154785b602ee0e90a958b3db
iRuxu <rx6@qq.com>
2021-03-25 20:23:01 +0800

    手滑

--- src/pages/Join.vue
+++ src/pages/Join.vue
@@ -169,7 +169,7 @@ export default {
                                 });
                                 this.status = true;
                                 this.$forceUpdate();
-                            }).finall(() => {
+                            }).finally(() => {
                                 this.processing = false
                             })
                         }
