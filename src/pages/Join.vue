13c5bc275fb8727e9c593fe73ce2ef29e0b20788
iRuxu <rx6@qq.com>
2021-10-23 04:32:59 +0800

    fix join link

--- src/pages/Join.vue
+++ src/pages/Join.vue
@@ -95,7 +95,7 @@
                         </div>
                     </div>
                     <div class="u-footer">
-                        <a href="/bbs/21805" target="_blank"
+                        <a href="/notice/32280" target="_blank"
                             ><i class="el-icon-info"></i>
                             <b>点击查看活动规则详情</b></a
                         >
