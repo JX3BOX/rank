6eaa61f4eae04886b5ae2e93f8b18035a302de6a
litthy <litthy@qq.com>
2024-02-26 15:05:59 +0800

    feat:process

---
+++ src/components/lover_battle.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="m-lover-battle">
+        <div class="m-team">
+            <el-image class="u-img" :src="url"></el-image>
+            <span class="u-name">队伍名称队伍名称队伍名称队伍名称</span>
+            <span class="u-status">胜</span>
+        </div>
+        <div class="m-team win">
+            <el-image class="u-img" :src="url"></el-image>
+            <span class="u-name">队伍名称队伍名称队伍名称队伍名称</span>
+            <span class="u-status">胜</span>
+        </div>
+        <a href="http://" target="_blank" class="u-detail">详情</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "battle",
+    data: function () {
+        return {};
+    },
+    computed: {},
+    methods: {},
+};
+</script>
+<style lang="less">
+.m-lover-battle {
+    .flex;
+    flex-direction: column;
+    gap: 6px;
+    .u-detail {
+        .size(40px,20px);
+        .fz(12px,20px);
+        .bold;
+        .x;
+        align-self: flex-end;
+        color: #f3c768;
+        letter-spacing: 3px;
+        background: #f3c76814;
+        border: 1px solid #f3c768;
+    }
+}
+</style>
--- src/views/Lover/Process.vue
+++ src/views/Lover/Process.vue
@@ -1,13 +1,25 @@
 <template>
     <div class="m-lover-process">
-
+        <div class="u-page-title">
+            <img class="u-img" :src="`${__imgRoot}process-title.png`" />
+        </div>
+        <div class="m-process-main">
+            <div class="m-step" v-for="item in 5" :key="item">
+                <div class="m-group">
+                    <LoverBattle />
+                    <LoverBattle />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import LoverBattle from "@/components/lover_battle.vue";
 export default {
     name: "LoverProcess",
     inject: ["__imgRoot"],
+    components: { LoverBattle },
     data: function () {
         return {};
     },
@@ -15,3 +27,6 @@ export default {
     methods: {},
 };
 </script>
+<style lang="less">
+@import "~@/assets/css/lover/process.less";
+</style>
