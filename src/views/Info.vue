19349f233d2a4381967c754d6b6e57239f7aefd3
TiAmo <361258177@qq.com>
2024-03-30 10:48:52 +0800

    fix: 时间轴

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,48 +1,45 @@
 <template>
     <!-- 活动规则与奖励设置 -->
     <div class="m-rank-info">
-        <div class="m-rank-btns">
-            <div class="m-time-shaft-box">
-                <h2>活动时间线</h2>
-                <div class="m-time-shaft-swiper-box">
-                    <swiper-container
-                        class="m-time-shaft-swiper"
-                        slides-per-view="auto"
-                        space-between="34"
-                        ref="timeShaftSwiperRef"
+        <div class="m-time-shaft-box" v-if="timeNode.length">
+            <h2>活动时间线</h2>
+            <div class="m-time-shaft-swiper-box">
+                <swiper-container
+                    class="m-time-shaft-swiper"
+                    slides-per-view="auto"
+                    :centered-slides="true"
+                    space-between="34"
+                    ref="timeShaftSwiperRef"
+                >
+                    <swiper-slide
+                        v-for="(item, index) in timeNode"
+                        class="m-shaft-item"
+                        :class="{
+                            'm-shaft-item__un_arrived': !item.reach,
+                        }"
+                        :key="index"
                     >
-                        <swiper-slide
-                            class="m-shaft-item"
-                            :class="{
-                                'm-shaft-item__un_arrived': index % 2,
-                            }"
-                            v-for="(item, index) in Array(20)"
-                            :key="index"
-                        >
-                            <p class="m-item__time">03-{{ index }}</p>
-                            <div class="m-item__decorate"></div>
-                            <div class="m-item__division"></div>
-                            <div class="m-item__desc">
-                                <span> 体服10人开放 </span>
-                            </div>
+                        <p class="m-item__time">{{ item.showTime }}</p>
+                        <div class="m-item__decorate"></div>
+                        <div class="m-item__division"></div>
+                        <div class="m-item__desc">
+                            <span>{{ item.text }}</span>
+                        </div>
 
-                            <div class="m-item__now" v-if="index == 5">
-                                <span>当前进度</span>
-                            </div>
-                        </swiper-slide>
-                    </swiper-container>
-                    <img
-                        class="m-time-shaft-swiper__arrow"
-                        @click="timeShaftChange('prev')"
-                        :src="`${timelineArrow}`"
-                    />
-                    <img
-                        @click="timeShaftChange('next')"
-                        class="m-time-shaft-swiper__arrow m-time-shaft-swiper__arrow_right"
-                        :src="`${timelineArrow}`"
-                    />
-                </div>
+                        <div class="m-item__now" v-if="item.isNow">
+                            <span>当前进度</span>
+                        </div>
+                    </swiper-slide>
+                </swiper-container>
+                <img class="m-time-shaft-swiper__arrow" @click="timeShaftChange('prev')" :src="`${timelineArrow}`" />
+                <img
+                    @click="timeShaftChange('next')"
+                    class="m-time-shaft-swiper__arrow m-time-shaft-swiper__arrow_right"
+                    :src="`${timelineArrow}`"
+                />
             </div>
+        </div>
+        <div class="m-rank-btns">
             <a class="u-btn u-btn-join" href="../join/" target="_blank" rel="noopener"></a>
             <!-- <a class="u-btn u-btn-lucky" href="/fb" target="_blank" rel="noopener">副本开荒攻略</a> -->
         </div>
@@ -53,11 +50,13 @@
 <script>
 import { register } from "swiper/swiper-element";
 import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+import { getBreadcrumb } from "@jx3box/jx3box-common/js/api_misc";
 export default {
     props: [],
     data: function () {
         return {
             timelineArrow: __imgPath + "image/rank/common/timeline_arrow.svg",
+            timeNode: [],
         };
     },
     computed: {
@@ -70,8 +69,30 @@ export default {
     },
     mounted: function () {},
     created: function () {
-        register();
-        // this.$refs.timeShaftSwiperRef.swiper.slideTo(10); swiper 移动到指定位置
+        getBreadcrumb("top_time_line").then((data) => {
+            data = JSON.parse(data);
+            if (`id-${this.$store.state.id}` in data) {
+                let nArr = data[`id-${this.$store.state.id}`].node;
+                const currentTime = new Date();
+                nArr.forEach((item) => {
+                    item.reach = new Date(item.time) <= currentTime;
+                });
+
+                var lastIndex = nArr
+                    .slice()
+                    .reverse()
+                    .findIndex((item) => item.reach);
+
+                if (lastIndex !== -1) {
+                    nArr[nArr.length - 1 - lastIndex].isNow = true;
+                }
+                this.timeNode = nArr;
+                register();
+                setTimeout(() => {
+                    this.$refs.timeShaftSwiperRef.swiper.slideTo(nArr.length - 1 - lastIndex);
+                }, 100);
+            }
+        });
     },
     methods: {
         timeShaftChange(type) {
