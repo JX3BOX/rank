dc995046213cb02cc1cc9d94a44de0b913af5092
xuan <litthy@qq.com>
2024-04-01 00:35:35 +0800

    fix: style

---
+++ src/components/time_line.vue
@@ -0,0 +1,194 @@
+<template>
+    <div class="w-time-line-box">
+        <h2>活动时间线</h2>
+        <div class="m-time-line-box">
+            <img class="u-arr u-arr-left" @click="scrollLeft" :src="arrow" />
+            <div class="m-timeline">
+                <ul ref="timeline" :style="{ left: listLeft + 'px' }" v-html="html"></ul>
+            </div>
+            <img class="u-arr u-arr-right" @click="scrollRight" :src="arrow" />
+        </div>
+    </div>
+</template>
+
+<script>
+import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
+export default {
+    name: "TimeLine",
+    props: {
+        html: {
+            type: String,
+            default: "",
+        },
+    },
+    data: function () {
+        return {
+            listLeft: 0,
+            currentIndex: 0,
+            arrow: `${__imgPath}image/rank/common/timeline_arrow.svg`,
+        };
+    },
+    watch: {
+        html: {
+            immediate: true,
+            handler(val) {
+                val && this.$nextTick(this.setLiPosition);
+            },
+        },
+    },
+    methods: {
+        setLiPosition() {
+            const list = this.$refs.timeline;
+            const liElements = list.querySelectorAll("li");
+            let left = 0;
+            for (let i = 0; i < liElements.length; i++) {
+                const li = liElements[i];
+                li.style.left = left + "px";
+                left += li.offsetWidth + 34;
+                if (i === this.currentIndex) {
+                    this.listLeft = -li.offsetLeft;
+                }
+            }
+        },
+        scrollLeft() {
+            const list = this.$refs.timeline;
+            const li = list.querySelector("li");
+            if (!li) return;
+            this.currentIndex = Math.max(0, this.currentIndex - 1);
+            this.setLiPosition();
+        },
+        scrollRight() {
+            const list = this.$refs.timeline;
+            const li = list.querySelector("li");
+            if (!li) return;
+            this.currentIndex = Math.min(this.currentIndex + 1, list.children.length - 1);
+            this.setLiPosition();
+        },
+    },
+};
+</script>
+
+<style lang="less">
+.w-time-line-box {
+    .pr;
+    .clip;
+    .mb(88px);
+    .w(100%);
+
+    h2 {
+        margin: 0;
+    }
+    .m-time-line-box {
+        .pr;
+        .flex;
+        .clip;
+        .h(150px);
+        gap: 10px;
+        align-items: center;
+        padding: 0 20px;
+        margin: 104px auto 146px auto;
+        box-sizing: border-box;
+        .m-timeline {
+            .clip;
+            .size(100%,142px);
+            .flex;
+            padding: 0 10px;
+            box-sizing: border-box;
+            align-items: center;
+        }
+
+        ul {
+            .pr;
+            .size(100%,1px);
+            margin: 0;
+            transition: left 0.2s;
+        }
+        li {
+            .pa;
+            .flex;
+            top: -56px;
+            flex-direction: column;
+            list-style: none;
+            align-items: center;
+            justify-content: center;
+            &::after {
+                content: "";
+                .pa;
+                .size(1980px,1px);
+                top: 57px;
+                background-color: #feecc5;
+            }
+            .u-time {
+                .fz(22px);
+                .bold;
+                color: rgba(254, 236, 197, 1);
+                &::after {
+                    content: "";
+                    .db;
+                    .auto(x);
+                    .size(17px);
+                    background-color: #feecc5;
+                    transform: rotate(45deg);
+                    border: 2px solid white;
+                    box-sizing: border-box;
+                }
+            }
+            .u-label {
+                .pr;
+                .z(1);
+                .pointer;
+                .fz(16px,42px);
+                .mt(20px);
+                height: 45px;
+                padding: 0 30px 0 34px;
+                color: #474747;
+                white-space: nowrap;
+                box-sizing: border-box;
+
+                &::after {
+                    content: "";
+                    .pa;
+                    .z(-1);
+                    .full;
+                    top: -2px;
+                    right: 0;
+                    bottom: 0;
+                    left: 0;
+                    background: linear-gradient(270deg, #f9e5bb 0%, #ffffff 100%);
+                    border-radius: 8px 0 10px 0;
+                    transform: skewX(-0.06turn);
+                }
+
+                &::before {
+                    content: "";
+                    .db;
+                    .pa;
+                    .z(2);
+                    .size(2px,14px);
+                    .auto(x);
+                    left: calc(50% - 1px);
+                    top: -14px;
+                    background: linear-gradient(180deg, rgba(254, 236, 197, 0) 0%, #fcf1da 100%);
+                }
+            }
+        }
+    }
+    .u-arr {
+        // .pa;
+        .pointer;
+        .size(35px);
+        .z(9);
+        bottom: 0;
+        &.u-arr-left {
+            left: -45px;
+        }
+        &.u-arr-right {
+            right: -45px;
+            transform: rotate(180deg);
+        }
+        &:hover {
+            filter: brightness(110%);
+        }
+    }
+}
+</style>
--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,44 +1,7 @@
 <template>
     <!-- 活动规则与奖励设置 -->
     <div class="m-rank-info">
-        <div class="m-time-shaft-box" v-if="timeNode.length">
-            <h2>活动时间线</h2>
-            <div class="m-time-shaft-swiper-box">
-                <swiper-container
-                    class="m-time-shaft-swiper"
-                    slides-per-view="auto"
-                    :centered-slides="true"
-                    space-between="34"
-                    ref="timeShaftSwiperRef"
-                >
-                    <swiper-slide
-                        v-for="(item, index) in timeNode"
-                        class="m-shaft-item"
-                        :class="{
-                            'm-shaft-item__un_arrived': !item.reach,
-                        }"
-                        :key="index"
-                    >
-                        <p class="m-item__time">{{ item.showTime }}</p>
-                        <div class="m-item__decorate"></div>
-                        <div class="m-item__division"></div>
-                        <div class="m-item__desc">
-                            <span>{{ item.text }}</span>
-                        </div>
-
-                        <div class="m-item__now" v-if="item.isNow">
-                            <span>当前进度</span>
-                        </div>
-                    </swiper-slide>
-                </swiper-container>
-                <img class="m-time-shaft-swiper__arrow" @click="timeShaftChange('prev')" :src="`${timelineArrow}`" />
-                <img
-                    @click="timeShaftChange('next')"
-                    class="m-time-shaft-swiper__arrow m-time-shaft-swiper__arrow_right"
-                    :src="`${timelineArrow}`"
-                />
-            </div>
-        </div>
+        <TimeLine :html="times" v-if="times" />
         <div class="m-rank-btns">
             <a class="u-btn u-btn-join" href="../join/" target="_blank" rel="noopener"></a>
             <!-- <a class="u-btn u-btn-lucky" href="/fb" target="_blank" rel="noopener">副本开荒攻略</a> -->
@@ -48,15 +11,17 @@
 </template>
 
 <script>
-import { register } from "swiper/swiper-element";
 import { __imgPath } from "@jx3box/jx3box-common/data/jx3box.json";
 import { getBreadcrumb } from "@jx3box/jx3box-common/js/api_misc";
+import TimeLine from "@/components/time_line.vue";
 export default {
-    props: [],
+    components: {
+        TimeLine,
+    },
     data: function () {
         return {
             timelineArrow: __imgPath + "image/rank/common/timeline_arrow.svg",
-            timeNode: [],
+            times: "",
         };
     },
     computed: {
@@ -67,31 +32,9 @@ export default {
             return this.$store.state.race.desc;
         },
     },
-    mounted: function () {},
     created: function () {
-        getBreadcrumb("top_time_line").then((data) => {
-            data = JSON.parse(data);
-            if (`id-${this.$store.state.id}` in data) {
-                let nArr = data[`id-${this.$store.state.id}`].node;
-                const currentTime = new Date();
-                nArr.forEach((item) => {
-                    item.reach = new Date(item.time) <= currentTime;
-                });
-
-                var lastIndex = nArr
-                    .slice()
-                    .reverse()
-                    .findIndex((item) => item.reach);
-
-                if (lastIndex !== -1) {
-                    nArr[nArr.length - 1 - lastIndex].isNow = true;
-                }
-                this.timeNode = nArr;
-                register();
-                setTimeout(() => {
-                    this.$refs.timeShaftSwiperRef.swiper.slideTo(nArr.length - 1 - lastIndex);
-                }, 100);
-            }
+        getBreadcrumb(`top_time_line_${this.id}`).then((data) => {
+            this.times = data;
         });
     },
     methods: {
@@ -103,11 +46,12 @@ export default {
             }
         },
     },
-    filters: {},
-    components: {},
 };
 </script>
 
 <style lang="less">
 @import "../assets/css/race_info.less";
+.m-rank-info .w-time-line-box h2{
+    margin: 0;
+}
 </style>
