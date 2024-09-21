3c97ee844b1dc8554aa4f2f52ef4f7fa4df869bb
zhusha <yumg@whsmwy.com>
2024-09-21 11:13:18 +0800

    remove: 多余插件

--- src/pages/surprise.js
+++ src/pages/surprise.js
@@ -13,12 +13,7 @@ Vue.use(JX3BOX_UI);
 
 import reporter from "@jx3box/jx3box-common/js/reporter";
 reporter.install(Vue);
-// 手动引入 ECharts 各模块来减小打包体积
-import ECharts from "vue-echarts"; //
-import "echarts/lib/chart/pie";
-import "echarts/lib/component/legend";
-import "echarts/lib/component/tooltip";
-Vue.component("v-chart", ECharts);
+
 // 数据与路由
 import router from "@/router/surprise";
 // import store from "@/store/surprise";
